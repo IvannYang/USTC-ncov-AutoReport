07383ca8f2c9f8cb2853db802ae1baf26c0f54cf
Ivan Yang <48438203+IvannYang@users.noreply.github.com>
2022-03-31 15:52:15 +0800

    Update ustclogin2.py

--- ustclogin2.py
+++ ustclogin2.py
@@ -1,38 +1,32 @@
-import pytz
-import re
-import sys
-import argparse
 from bs4 import BeautifulSoup
 import requests
-import json
 import numpy as np
+from urllib.parse import unquote
+headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
 class Login:
-    def __init__(self, stuid, password, origin, service, exam):
+    def __init__(self, stuid, password, service):
         self.stuid=stuid
         self.password=password
-        self.origin=origin
         self.service=service
-        self.exam=exam
-
+    
     def passport(self):
-        data=self.session.get('https://passport.ustc.edu.cn/login?service='+self.service)
+        data=self.session.get('https://passport.ustc.edu.cn/login?service='+self.service,headers=headers)
         data=data.text
         data = data.encode('ascii','ignore').decode('utf-8','ignore')
         soup = BeautifulSoup(data, 'html.parser')
         CAS_LT = soup.find("input", {"name": "CAS_LT"})['value']
-        self.session.get('https://passport.ustc.edu.cn/validatecode.jsp?type=login',stream=True)
         data = {
             'model': 'uplogin.jsp',
-            'service': self.service,
+            'service': unquote(self.service),
             'warn': '',
-            'showCode': '0',
+            'showCode': '',
             'username': self.stuid,
             'password': str(self.password),
             'button': '',
             'CAS_LT':CAS_LT,
             'LT':''
         }
-        self.session.post('https://passport.ustc.edu.cn/login', data=data)
+        self.result=self.session.post('https://passport.ustc.edu.cn/login', data=data,headers=headers)
         
     def login(self):
         self.session=requests.Session()
@@ -41,12 +35,10 @@ class Login:
         while (not loginsuccess) and retrycount:
             self.passport()
             self.cookies = self.session.cookies
-            self.result = self.session.get(self.origin)
             retrycount = retrycount - 1
-            if self.result.url != self.exam:
+            if self.result.url=='https://passport.ustc.edu.cn/login':
                 print("Login Failed! Retry...")
             else:
                 print("Login Successful!")
                 loginsuccess = True
         return loginsuccess
-        
