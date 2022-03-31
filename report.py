638af8f05a0314bbd69639593686deec2cf96824
Ivan Yang <48438203+IvannYang@users.noreply.github.com>
2022-03-31 15:45:14 +0800

    Update report.py

--- report.py
+++ report.py
@@ -1,11 +1,17 @@
 import time
-import datetime
 import re
 import argparse
 from bs4 import BeautifulSoup
 import json
 import pytz
 from ustclogin import Login
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+SHA_TZ = timezone( #北京时间
+    timedelta(hours=8),
+    name='Asia/Shanghai',
+)
 class Report(object):
     def __init__(self, stuid, password, data_path):
         self.stuid = stuid
@@ -13,14 +19,14 @@ class Report(object):
         self.data_path = data_path
 
     def report(self):
-        login=Login(self.stuid,self.password,'https://weixine.ustc.edu.cn/2020','https://weixine.ustc.edu.cn/2020/caslogin','https://weixine.ustc.edu.cn/2020/home')
+        login=Login(self.stuid,self.password,'https://weixine.ustc.edu.cn/2020/caslogin')
         if login.login():
             data = login.result.text
             data = data.encode('ascii','ignore').decode('utf-8','ignore')
             soup = BeautifulSoup(data, 'html.parser')
             token = soup.find("input", {"name": "_token"})['value']
 
-            with open(self.data_path, "r+") as f:
+            with open(self.data_path, "r+", encoding='utf-8') as f:
                 data = f.read()
                 data = json.loads(data)
                 data["_token"]=token
@@ -29,7 +35,7 @@ class Report(object):
                 'origin': 'https://weixine.ustc.edu.cn',
                 'upgrade-insecure-requests': '1',
                 'content-type': 'application/x-www-form-urlencoded',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39',
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'referer': 'https://weixine.ustc.edu.cn/2020/',
                 'accept-language': 'zh-CN,zh;q=0.9',
@@ -49,12 +55,36 @@ class Report(object):
                 date = pattern.search(token.text).group()
                 print("Latest report: " + date)
                 date = date + " +0800"
-                reporttime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
-                timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+                reporttime = datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
+                timenow = datetime.now(pytz.timezone('Asia/Shanghai'))
                 delta = timenow - reporttime
                 print("{} second(s) before.".format(delta.seconds))
                 if delta.seconds < 120:
                     flag = True
+            headers={
+                'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39'}
+            data=login.session.get('https://weixine.ustc.edu.cn/2020/apply/daliy',headers=headers).text
+            data = data.encode('ascii','ignore').decode('utf-8','ignore')
+            soup = BeautifulSoup(data, 'html.parser')
+            token = soup.find("input", {"name": "_token"})['value']
+            data=login.session.get('https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3',headers=headers).text
+            data = data.encode('ascii','ignore').decode('utf-8','ignore')
+            soup = BeautifulSoup(data, 'html.parser')
+            start_date = soup.find("input", {"id": "start_date"})['value']
+            end_date = soup.find("input", {"id": "end_date"})['value']
+            data={
+                '_token':token,
+                'start_date':start_date,
+                'end_date':end_date,
+                'return_college[]':'西校区',
+                'return_college[]':'高新校区',
+                't':'3'}
+            post=login.session.post('https://weixine.ustc.edu.cn/2020/apply/daliy/post',data=data)
+            if post.url=='\
+https://weixine.ustc.edu.cn/2020/apply_total?t=d' and flag==True:
+                flag=True
+            else:
+                flag=False
             if flag == False:
                 print("Report FAILED!")
             else:
